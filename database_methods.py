5e88ee01521f623424b9d11466169e4ae35cfc17
dan <danilsocol-2002@mail.ru>
2022-07-04 21:44:58 +0500

    add adm_func and profile(first meet)

--- database_methods.py
+++ database_methods.py
@@ -21,3 +21,19 @@ def get_avito_list(user_id, request, lower_bound, upper_bound):
     data = p.parse_20_cards(request, lower_bound, upper_bound)
     for i in range(0, len(data)):
         print(data[i])
+
+    #TODO: проверить есть ли id чата в бд
+def check_first_start(user_id):
+    return #bool
+
+    #TODO: Выдать список новых пользователей зарегистрированных за промежуток времени
+def get_list_rookie(date):
+    return list
+
+    #TODO: Выдать список активных за промежуток времени
+def get_list_act(date):
+    return list
+
+    #TODO: Кол-во запросов
+def get_list_requests():
+    return list
